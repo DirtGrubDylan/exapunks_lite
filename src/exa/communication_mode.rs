e60df7c31752de483c1b0c9895e297ed1c7fe41c
Dylan Hicks <dylanhicks@google.com>
2023-11-27 17:13:46 +0000

    Create the EXA CommunicationMode enum.

---
+++ src/exa/communication_mode.rs
@@ -0,0 +1,10 @@
+/// This enum dictates which communication mode the [`Exa`] is in.
+///
+/// * Global - The "M" register can be written/read by all other EXAs also in Global mode.
+/// * Local - The "M" register can be written/read by all other EXAs in the same [`Host`] that are
+/// also in Local mode.
+#[derive(Debug, Clone, Copy, Eq, PartialEq)]
+pub enum CommunicationMode {
+    Global,
+    Local,
+}
