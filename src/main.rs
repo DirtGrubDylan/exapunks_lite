75f52f11d75718e7bee95694c119dcac2e878c3d
Dylan Hicks <dman1366@gmail.com>
2023-11-27 09:26:59 -0700

    Clean up main a bit.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 pub mod value;
 
+use value::Value;
+
 fn main() {
-    let number = value::Value::Number(-127);
+    let number = Value::Number(-127);
 
     println!("Hello, world!");
     println!("Here is my number: {number:?}");
