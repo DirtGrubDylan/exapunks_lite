600e4ad72558fb8d2e6c7db27f808c6369461021
Dylan Hicks <dman1366@gmail.com>
2023-11-27 09:15:35 -0700

    Add the FromStr implementation.
    I also removed all of the tests for the math operator overloads. This was unneccessary.

--- src/value.rs
+++ src/value.rs
@@ -1,6 +1,6 @@
 use std::cmp::{Eq, Ord, PartialEq, PartialOrd};
 use std::convert::{From, Into};
-use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Rem, RemAssign, Sub, SubAssign};
+use std::str::FromStr;
 
 /// A `Value` is used to hold several types of information: number, keyword, register id, and a
 /// label id. Each type is used by an [`Exa`] to perform their tasks. Whether it is storing keywords
@@ -11,7 +11,7 @@ use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Rem, RemAssign, S
 /// and numbers.
 ///
 /// A [`Register`] can hold on to a number or keyword value.
-#[derive(Debug)]
+#[derive(Debug, Eq, PartialEq, Clone)]
 pub enum Value {
     Number(isize),
     Keyword(String),
@@ -19,17 +19,9 @@ pub enum Value {
     LabelId(String),
 }
 
-impl PartialEq for Value {
-    fn eq(&self, other: &Self) -> bool {
-        match (self, other) {
-            (Value::Number(lhs), Value::Number(rhs)) => lhs == rhs,
-            (Value::Keyword(lhs), Value::Keyword(rhs)) => lhs == rhs,
-            _ => panic!("Cannot check equivalence for {self:?} and {other:?}!"),
-        }
-    }
-}
-
-impl Eq for Value {}
+/// A dummy struct to indicate that there was an error on the [`FromStr`] implementations.
+#[derive(Debug, Eq, PartialEq, Clone)]
+pub struct ValueParseError;
 
 impl From<isize> for Value {
     fn from(input: isize) -> Self {
@@ -46,132 +38,58 @@ impl Into<isize> for Value {
     }
 }
 
+impl Into<String> for Value {
+    fn into(self) -> String {
+        match self {
+            Self::Number(number) => number.to_string(),
+            Self::Keyword(keyword) => keyword.clone(),
+            Self::RegisterId(register_id) => register_id.clone(),
+            Self::LabelId(label_id) => label_id.clone(),
+        }
+    }
+}
+
+impl FromStr for Value {
+    type Err = ValueParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.parse::<isize>() {
+            _ if s.is_empty() => Err(ValueParseError),
+            Ok(number) => Ok(Value::Number(number)),
+            Err(_) => Ok(Value::Keyword(s.to_string())),
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::Value;
 
     #[test]
-    fn test_new() {
-        unimplemented!()
+    fn test_parse_from_str_to_number() {
+        let number_string = "-127".to_string();
+
+        let number = number_string.parse().unwrap();
+
+        assert_eq!(Value::Number(-127), number);
     }
 
     #[test]
-    fn test_add_number() {
-        unimplemented!()
+    fn test_parse_from_str_to_keyword() {
+        let keyword_string: String = "keyword".to_string();
+
+        let keyword = keyword_string.parse().unwrap();
+
+        assert_eq!(Value::Keyword("keyword".to_string()), keyword);
     }
 
     #[test]
-    fn test_add_non_number_panics() {
-        unimplemented!()
-    }
+    fn test_parse_from_empty_str_err() {
+        let empty_string = String::new();
 
-    #[test]
-    fn test_subtract_number() {
-        unimplemented!()
-    }
+        let value_err = empty_string.parse::<Value>();
 
-    #[test]
-    fn test_subtract_non_number_panics() {
-        unimplemented!()
-    }
-
-    #[test]
-    fn test_divide_number() {
-        unimplemented!()
-    }
-
-    #[test]
-    fn test_divide_non_number_panics() {
-        unimplemented!()
-    }
-
-    #[test]
-    fn test_multiply_number() {
-        unimplemented!()
-    }
-
-    #[test]
-    fn test_multiply_non_number_panics() {
-        unimplemented!()
-    }
-
-    #[test]
-    fn test_modulo_number() {
-        unimplemented!()
-    }
-
-    #[test]
-    fn test_modulo_non_number_panics() {
-        unimplemented!()
-    }
-
-    #[test]
-    fn test_greater_than_number() {
-        unimplemented!()
-    }
-
-    #[test]
-    fn test_greater_than_keyword() {
-        unimplemented!()
-    }
-
-    #[test]
-    fn test_greater_than_non_number_non_keyword_panics() {
-        unimplemented!()
-    }
-
-    #[test]
-    fn test_less_than_number() {
-        unimplemented!()
-    }
-
-    #[test]
-    fn test_less_than_keyword() {
-        unimplemented!()
-    }
-
-    #[test]
-    fn test_less_than_non_number_non_keyword_panics() {
-        unimplemented!()
-    }
-
-    #[test]
-    fn test_equal_to_number() {
-        let first = Value::Number(128);
-        let second = Value::Number(128);
-        let third = Value::Number(-128);
-
-        assert_eq!(first, second);
-        assert_ne!(first, third);
-    }
-
-    #[test]
-    fn test_equal_to_keyword() {
-        let first = Value::Keyword("first".to_string());
-        let second = Value::Keyword("first".to_string());
-        let third = Value::Keyword("third".to_string());
-
-        assert_eq!(first, second);
-        assert_ne!(first, third);
-    }
-
-    #[test]
-    #[should_panic]
-    fn test_equal_to_non_number_non_keyword_panics() {
-        let first = Value::Keyword("first".to_string());
-        let second = Value::Number(128);
-
-        assert_eq!(first, second);
-    }
-
-    #[test]
-    fn test_from_str_to_number() {
-        unimplemented!()
-    }
-
-    #[test]
-    fn test_from_str_to_keyword() {
-        unimplemented!()
+        assert!(value_err.is_err());
     }
 
     #[test]
@@ -193,17 +111,28 @@ mod tests {
     }
 
     #[test]
+    #[should_panic]
     fn test_from_non_number_to_isize_panics() {
-        unimplemented!()
+        let keyword = Value::Keyword("keyword".to_string());
+
+        let _to_isize: isize = keyword.into();
     }
 
     #[test]
     fn test_from_number_to_string() {
-        unimplemented!()
+        let number = Value::Number(-127);
+
+        let number_string: String = number.into();
+
+        assert_eq!("-127".to_string(), number_string);
     }
 
     #[test]
     fn test_from_non_number_to_string() {
-        unimplemented!()
+        let keyword = Value::Keyword("keyword".to_string());
+
+        let keyword_string: String = keyword.into();
+
+        assert_eq!("keyword".to_string(), keyword_string);
     }
 }
