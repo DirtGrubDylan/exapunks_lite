be57a1e6ae6ca993944050982a942efda6474e27
Dylan Hicks <dman1366@gmail.com>
2025-01-10 09:38:26 -0700

    Add program to main.

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ pub mod util;
 pub mod value;
 
 use crate::program::instruction::Instruction;
+use crate::program::Program;
 use value::Value;
 
 fn main() {
@@ -12,8 +13,10 @@ fn main() {
     let rhs = Value::Number(128);
     let destination = Value::RegisterId("X".to_string());
     let add_instruction = Instruction::Add(lhs.clone(), rhs, destination);
+    let program = Program::new_from_file("test_files/simple_program.exa").unwrap();
 
     println!("Hello, world!");
     println!("Here is my number: {lhs:?}");
     println!("Here is my add instruction: {add_instruction:?}");
+    println!("Here is my simple program:\n{program}");
 }
