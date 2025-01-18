64a779a1eab5fb9b49a7282a642e76a2a49c5f90
Dylan Hicks <dman1366@gmail.com>
2025-01-17 18:57:12 -0700

    Implement `Link` constructor.

--- src/host/link.rs
+++ src/host/link.rs
@@ -1,5 +1,5 @@
 use std::cell::RefCell;
-use std::rc::Weak;
+use std::rc::{Rc, Weak};
 
 use super::Host;
 
@@ -14,6 +14,22 @@ pub struct Link {
 }
 
 impl Link {
+    /// Creates a new link with given gate ids and host references.
+    pub fn new(
+        lhs_id: &str,
+        lhs_host: &Rc<RefCell<Host>>,
+        rhs_id: &str,
+        rhs_host: &Rc<RefCell<Host>>,
+    ) -> Self {
+        Link {
+            lhs_id: lhs_id.to_string(),
+            lhs_host: Rc::downgrade(lhs_host),
+            rhs_id: rhs_id.to_string(),
+            rhs_host: Rc::downgrade(rhs_host),
+            occupied: false,
+        }
+    }
+
     /// Returns the relative [`Host`] reference for the given gate id if possible.
     #[must_use]
     pub fn destination(&self, gate_id: &str) -> Option<Weak<RefCell<Host>>> {
