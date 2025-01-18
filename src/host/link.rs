218545e59206afbbd9f3bcd3dd8c2ea29329a602
Dylan Hicks <dman1366@gmail.com>
2025-01-17 18:52:21 -0700

    Implement `Link` struct

--- src/host/link.rs
+++ src/host/link.rs
@@ -5,11 +5,24 @@ use super::Host;
 
 /// A link has two gates, identified by id's, that point to two [`Host`]s
 #[derive(Debug, Clone)]
-#[allow(dead_code)]
 pub struct Link {
     lhs_id: String,
     lhs_host: Weak<RefCell<Host>>,
     rhs_id: String,
     rhs_host: Weak<RefCell<Host>>,
-    occupied: bool,
+    pub occupied: bool,
+}
+
+impl Link {
+    /// Returns the relative [`Host`] reference for the given gate id if possible.
+    #[must_use]
+    pub fn destination(&self, gate_id: &str) -> Option<Weak<RefCell<Host>>> {
+        if self.lhs_id == gate_id {
+            Some(self.lhs_host.clone())
+        } else if self.rhs_id == gate_id {
+            Some(self.rhs_host.clone())
+        } else {
+            None
+        }
+    }
 }
